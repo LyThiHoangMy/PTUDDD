9d52579a002d4e2c5946aaf41bf79cae61ed83db
LyThiHoangMy <51900137@student.tdtu.edu.vn>
2022-12-07 18:01:26 +0700

    Create Home Screen

--- DemoApp/app/src/main/java/com/example/cvasport/MainActivity.java
+++ DemoApp/app/src/main/java/com/example/cvasport/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.cvasport;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewHome;
+    NavigationView navigationView;
+    ListView listViewHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Mapping();
+    }
+
+    private void Mapping() {
+        toolbar = findViewById(R.id.toolbarHome);
+        viewFlipper = findViewById(R.id.viewFlipperHome);
+        recyclerViewHome = findViewById(R.id.rcvHome);
+        navigationView = findViewById(R.id.navigViewHome);
+        listViewHome = findViewById(R.id.lvHome);
     }
 }
