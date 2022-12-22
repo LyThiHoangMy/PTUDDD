888097727e19ec43566df30536ef0fa0d0d8013e
LyThiHoangMy <51900137@student.tdtu.edu.vn>
2022-12-22 13:45:06 +0700

    Create User Profile and Logout

--- CVA/app/src/main/java/com/example/cva/Fragment/ProfileFragment.java
+++ CVA/app/src/main/java/com/example/cva/Fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.cva.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,38 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.cva.Activity.LoginActivity;
 import com.example.cva.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private View mView;
+    TextView textLogout;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -61,6 +54,20 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        mView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        textLogout = mView.findViewById(R.id.textLogout);
+
+        logOut();
+        return mView;
+    }
+
+    private void logOut() {
+        textLogout.setOnClickListener((view) ->{
+            FirebaseAuth.getInstance().signOut();
+            Intent login = new Intent(getActivity(), LoginActivity.class);
+            startActivity(login);
+            getActivity().finish();
+        });
     }
 }
